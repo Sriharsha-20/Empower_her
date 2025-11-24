37e0b8b5133676361e1bf615ec0e9ba32deb1e3d
Sriharsha-20 <sunkasriharsha95@gmail.com>
2025-11-24 23:16:27 +0530

    third

---
+++ Module2/modern JS/Multi-level array/multiLevel.js
@@ -0,0 +1,30 @@
+const people = [
+    {
+        name: "Alice",
+        address: {
+            city: "New York",
+            street: {
+                name: "Broadway",
+                number: 123
+            }
+        }
+    },
+    {
+        name: "Bob",
+        address: {
+            city: "Los Angeles",
+            street: {
+                name: "Sunset Boulevard",
+                number: 456
+            }
+        }
+    }
+];
+
+function formatPeopleData(arr) {
+    return arr.map(({ name, address: { city, street: { name: streetName } } }) => {
+        return `${name} lives in ${city} on ${streetName}`;
+    });
+}
+const result = formatPeopleData(people);
+console.log(result);
